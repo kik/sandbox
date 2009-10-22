f41bc1346d4491f937b3fb78da38a07c746acf1c
kik <kik314@gmail.com>
2009-10-22 20:21:45 +0900

    cleanup

--- coq/ivt.v
+++ coq/ivt.v
@@ -8,31 +8,6 @@ Definition ball x r y := x - r < y < x + r.
 Definition subset (E F: R -> Prop) :=
   forall y, E y -> F y.
 
-Lemma ball_le_subset: forall x r1 r2,
-  r1 <= r2 -> subset (ball x r1) (ball x r2).
-Proof.
-  unfold subset, ball.
-  intros.
-  destruct H0.
-  constructor; fourier.
-Qed.
-
-Lemma ball_min_subset_l: forall x r1 r2,
-  subset (ball x (Rmin r1 r2)) (ball x r1).
-Proof.
-  intros.
-  apply ball_le_subset.
-  apply Rmin_l.
-Qed.
-
-Lemma ball_min_subset_r: forall x r1 r2,
-  subset (ball x (Rmin r1 r2)) (ball x r2).
-Proof.
-  intros.
-  apply ball_le_subset.
-  apply Rmin_r.
-Qed.
-
 Definition invimg (f: R -> R) (E: R -> Prop) :=
   fun y => E (f y).
 
@@ -42,19 +17,18 @@ Definition continuity (f: R -> R) :=
   { delta: R | delta > 0 /\ subset (ball x delta) (invimg f (ball (f x) eps)) }.
 
 Definition inner_pt (E: R -> Prop) x :=
-  { delta: R | delta > 0 /\ subset (ball x delta) E}.
+  exists delta, delta > 0 /\ subset (ball x delta) E.
 
 Definition complement (E: R -> Prop) :=
   fun x => ~ E x.
 
 Lemma sup_def_1: forall (E: R -> Prop) s, is_lub E s ->
-  forall eps, eps > 0 -> forall x, s < x < s+eps -> ~E x.
+  forall x, s < x -> (complement E) x.
 Proof.
-  unfold is_lub.
+  unfold is_lub, complement.
   intros.
   intro.
   destruct H.
-  destruct H1.
   assert (x <= s) by auto.
   fourier.
 Qed.
@@ -80,28 +54,25 @@ Proof.
   fourier.
 Qed.
 
-Lemma sup_not_inner: forall (E: R -> Prop) s, is_lub E s ->
-  inner_pt E s -> False.
+Lemma sup_not_inner: forall (E: R -> Prop) s, is_lub E s ->~inner_pt E s.
 Proof.
-  intros.
+  intros. intro.
   destruct H0.
-  destruct a.
+  destruct H0.
   assert (E (s + x * /2)).
   unfold subset in H1.
   apply H1.
   unfold ball. constructor; fourier.
-  assert (H3 := sup_def_1 E s H x H0).
-  apply H3 with (s + x * / 2).
-  constructor; fourier.
+  assert (s < s + x * / 2) by fourier.
+  assert (H4 := sup_def_1 E s H (s + x * / 2) H3).
   auto.
 Qed.
 
-Lemma sup_not_outer: forall (E: R -> Prop) s, is_lub E s ->
-  inner_pt (complement E) s -> False.
+Lemma sup_not_outer: forall (E: R -> Prop) s, is_lub E s -> ~inner_pt (complement E) s.
 Proof.
-  intros.
+  intros. intro.
   destruct H0.
-  destruct a.
+  destruct H0.
   unfold subset in H1.
   assert (forall y, s - x < y <= s -> complement E y).
   intros.
@@ -141,39 +112,33 @@ Proof.
   constructor; auto.
   constructor; auto.
 
-  destruct (total_order_T (f z) 0) as [[Hpos|Hzero]|Hneg].
+  destruct (Rtotal_order (f z) 0) as [Hpos|[Heq|Hneg]].
   (* Hpos *)
   apply False_ind.
   assert (inner_pt A z).
 
-  assert (z < y).
-  destruct (Rtotal_order z y); auto.
-  destruct H3.
-  rewrite H3 in Hpos. fourier.
-  destruct Hxzy. fourier.
-
   set (eps := - f z).
   assert (Hepspos: eps > 0).
   unfold eps; auto with real.
-  destruct (H z eps Hepspos) as [delta1 [Hdelta1pos Hdelta1]].
+  destruct (H z eps Hepspos) as [delta [Hdeltapos Hdelta]].
 
-  set (delta2 := y - z).
-  assert (Hdelta2pos : delta2 > 0).
-  unfold delta2; fourier.
-  set (delta := Rmin delta1 delta2).
   exists delta.
   constructor.
-  unfold delta. auto using Rmin_Rgt_r.
+  auto using Rmin_Rgt_r.
 
   unfold subset.
   intro t. intros.
   constructor.
-  generalize (ball_min_subset_r z delta1 delta2 t H4).
-  unfold ball, delta2. intros [Ht1 Ht2]. fourier.
 
-  generalize (ball_min_subset_l z delta1 delta2 t H4).
-  intros.
-  generalize (Hdelta1 t H5).
+  apply Rnot_lt_le.
+  intro.
+  assert(ball z delta y).
+  unfold ball. destruct H3. destruct Hxzy. constructor; fourier.
+  assert (H6 := Hdelta y H5).
+  unfold invimg, ball, eps in H6.
+  destruct H6. fourier.
+
+  generalize (Hdelta t H3).
   unfold invimg, ball, eps. intros [Ht1 Ht2]. fourier.
 
   exact (sup_not_inner A z Hsup H3).
@@ -197,3 +162,4 @@ Proof.
   destruct H5. destruct H4. fourier.
   exact (sup_not_outer A z Hsup H3).
 Qed.
+
