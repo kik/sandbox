b38c20593e9a6e9e66ec5099e690b47eb3c38723
kikuchi <kikuchi@etch-gp02.local.lepidum.net>
2010-07-09 23:39:09 +0900

    ntarai

--- coq/ntarai.v
+++ coq/ntarai.v
@@ -2,7 +2,14 @@ Require Import ZArith.
 
 Open Scope Z_scope.
 
-Definition oZ := option Z.
+Inductive denote (A: Type) : Type :=
+| V : A -> denote A
+| B : denote A.
+
+Implicit Arguments V [A].
+Implicit Arguments B [A].
+
+Definition Z' := denote Z.
 
 Fixpoint Yn {A: Type} n (init: A) (recur: A -> A) : A :=
 match n with
@@ -10,54 +17,56 @@ match n with
 | S n' => recur (Yn n' init recur)
 end.
 
-Definition minus_1 (x: oZ) :=
+Definition minus_1 (x: Z') : Z' :=
 match x with
-| Some x => Some (x - 1)
-| None => None
+| V x => V (x - 1)
+| B => B
 end.
 
-Definition ntarai5_init (a b c d e: oZ) : option Z := None.
-Definition ntarai5_rec f (a b c d e: oZ) : option Z :=
+Definition ntarai5_init (a b c d e: Z') : Z' := B.
+Definition ntarai5_rec f (a b c d e: Z') : Z' :=
 match a, b with
-| Some av, Some bv =>
+| V av, V bv =>
   if Z_le_dec av bv then b else
   f (f (minus_1 a) b c d e)
     (f (minus_1 b) c d e a)
     (f (minus_1 c) d e a b)
     (f (minus_1 d) e a b c)
     (f (minus_1 e) a b c d)
-| _, _ => None 
+| _, _ => B 
 end.
 
 Definition ntarai5 n := Yn n ntarai5_init  ntarai5_rec.
 
 Inductive Tarai_value:
-     nat -> oZ -> oZ -> oZ -> oZ -> oZ -> oZ -> Prop :=
-| Tarai_value_le n a b c d e: a <= b ->
-     Tarai_value (S n) (Some a) (Some b) c d e (Some b)
-| Tarai_value_gt n a b c d e v1 v2 v3 v4 v5 vr: ~a <= b ->
-     Tarai_value n (minus_1 (Some a)) (Some b) c d e v1 ->
-     Tarai_value n (minus_1 (Some b)) c d e (Some a) v2 ->
-     Tarai_value n (minus_1 c) d e (Some a) (Some b) v3 ->
-     Tarai_value n (minus_1 d) e (Some a) (Some b) c v4 ->
-     Tarai_value n (minus_1 e) (Some a) (Some b) c d v5 ->
-     Tarai_value n v1 v2 v3 v4 v5 vr ->
-     Tarai_value (S n) (Some a) (Some b) c d e vr
+     nat -> Z' -> Z' -> Z' -> Z' -> Z' -> Z' -> Prop :=
+| Tarai_value_le n a b c d e
+     (Hle: a <= b):
+     Tarai_value (S n) (V a) (V b) c d e (V b)
+| Tarai_value_gt n a b c d e v1 v2 v3 v4 v5 vr
+     (Hnle: ~a <= b)
+     (Hv1: Tarai_value n (minus_1 (V a)) (V b) c d e v1)
+     (Hv2: Tarai_value n (minus_1 (V b)) c d e (V a) v2)
+     (Hv3: Tarai_value n (minus_1 c) d e (V a) (V b) v3)
+     (Hv4: Tarai_value n (minus_1 d) e (V a) (V b) c v4)
+     (Hv5: Tarai_value n (minus_1 e) (V a) (V b) c d v5)
+     (Hvr: Tarai_value n v1 v2 v3 v4 v5 vr):
+     Tarai_value (S n) (V a) (V b) c d e vr
 | Tarai_value_none_1 a b c d e:
-     Tarai_value O a b c d e None
+     Tarai_value O a b c d e B
 | Tarai_value_none_2 n b c d e:
-     Tarai_value n None b c d e None
+     Tarai_value n B b c d e B
 | Tarai_value_none_3 n a c d e:
-     Tarai_value n a None c d e None.
+     Tarai_value n a B c d e B.
 
 Lemma lem_step_l: forall n a b c d e v,
-   (if Z_le_dec a b then Some b else
-    (ntarai5 n (ntarai5 n (Some (a - 1)) (Some b) c d e)
-                  (ntarai5 n (Some (b - 1)) c d e (Some a))
-                  (ntarai5 n (minus_1 c) d e (Some a) (Some b))
-                  (ntarai5 n (minus_1 d) e (Some a) (Some b) c)
-                  (ntarai5 n (minus_1 e) (Some a) (Some b) c d))) = v ->
-   ntarai5 (S n) (Some a) (Some b) c d e = v.
+   (if Z_le_dec a b then V b else
+    (ntarai5 n (ntarai5 n (V (a - 1)) (V b) c d e)
+                  (ntarai5 n (V (b - 1)) c d e (V a))
+                  (ntarai5 n (minus_1 c) d e (V a) (V b))
+                  (ntarai5 n (minus_1 d) e (V a) (V b) c)
+                  (ntarai5 n (minus_1 e) (V a) (V b) c d))) = v ->
+   ntarai5 (S n) (V a) (V b) c d e = v.
 Proof.
   intros.
   exact H.
@@ -65,13 +74,13 @@ Qed.
 
 Lemma lem_step_r: forall n a b c d e v,
    v =
-   (if Z_le_dec a b then Some b else
-    (ntarai5 n (ntarai5 n (Some (a - 1)) (Some b) c d e)
-                  (ntarai5 n (Some (b - 1)) c d e (Some a))
-                  (ntarai5 n (minus_1 c) d e (Some a) (Some b))
-                  (ntarai5 n (minus_1 d) e (Some a) (Some b) c)
-                  (ntarai5 n (minus_1 e) (Some a) (Some b) c d))) ->
-   v = ntarai5 (S n) (Some a) (Some b) c d e.
+   (if Z_le_dec a b then V b else
+    (ntarai5 n (ntarai5 n (V (a - 1)) (V b) c d e)
+                  (ntarai5 n (V (b - 1)) c d e (V a))
+                  (ntarai5 n (minus_1 c) d e (V a) (V b))
+                  (ntarai5 n (minus_1 d) e (V a) (V b) c)
+                  (ntarai5 n (minus_1 e) (V a) (V b) c d))) ->
+   v = ntarai5 (S n) (V a) (V b) c d e.
 Proof.
   intros.
   exact H.
@@ -112,16 +121,11 @@ Proof.
   apply Tarai_value_le. auto.
   eapply Tarai_value_gt.
   auto.
-  apply IHn.
-  reflexivity.
-  apply IHn.
-  reflexivity.
-  apply IHn.
-  reflexivity.
-  apply IHn.
-  reflexivity.
-  apply IHn.
-  reflexivity.
+  apply IHn. reflexivity.
+  apply IHn. reflexivity.
+  apply IHn. reflexivity.
+  apply IHn. reflexivity.
+  apply IHn. reflexivity.
   apply IHn.
   unfold ntarai5 in H. simpl in H.
   destruct (Z_le_dec a b); [contradiction|].
@@ -132,61 +136,91 @@ Proof.
   constructor.
 Qed.
 
-Definition c_le (a b c d e a' b' c' d' e': option Z) :=
-  (a = a' \/ a = None) /\
-  (b = b' \/ b = None) /\
-  (c = c' \/ c = None) /\
-  (d = d' \/ d = None) /\
-  (e = e' \/ e = None).
+Definition Z'_le (x y: Z') := x = y \/ x = B.
+Definition Z5'_le (a b c d e a' b' c' d' e': Z') :=
+  Z'_le a a' /\  Z'_le b b' /\  Z'_le c c' /\  Z'_le d d' /\  Z'_le e e' .
 
-Lemma lem_none: forall n a b c d e a' b' c' d' e' v,
-  c_le a b c d e a' b' c' d' e' ->
-  Tarai_value n a b c d e (Some v) ->
-  Tarai_value n a' b' c' d' e' (Some v).
+Lemma lem_contin: forall n a b c d e v a' b' c' d' e' v',
+  Z5'_le a b c d e a' b' c' d' e' ->
+  Tarai_value n a b c d e v ->
+  Tarai_value n a' b' c' d' e' v' -> Z'_le v v'.
 Proof.
   induction n.
   intros. inversion H0.
-
+    right. auto.
+    right. auto.
+    right. auto.
   intros.
-  red in H.
-  inversion H0.
-  rewrite <- H3 in H.
-  rewrite <- H4 in H.
-  destruct H as [?[?[?[??]]]].
-  destruct H; [|congruence].
-  destruct H9; [|congruence].
-  rewrite <- H.
-  rewrite <- H9.
-  rewrite H2.
+  destruct H as [Ha[Hb[Hc[Hd He]]]].
+  destruct a as [a|].
+  destruct b as [b|].
+  destruct Ha as [Ha|?]; [|congruence].
+  destruct Hb as [Hb|?]; [|congruence].
+  rewrite <- Ha in H1.
+  rewrite <- Hb in H1.
+  inversion H0; inversion H1; try contradiction.
+  left. auto.
+  apply (IHn v1 v2 v3 v4 v5 v v0 v6 v7 v8 v9 v'); auto.
+  red.
+  assert (forall x, Z'_le x x). left. auto.
+  assert (forall x y, Z'_le x y -> Z'_le (minus_1 x) (minus_1 y)).
+    intros.
+    destruct H16.
+    rewrite H16.
+    auto.
+    rewrite H16.
+    right.
+    auto.
   constructor.
-  auto.
-  
+  apply (IHn (minus_1 (V a)) (V b) c d e v1
+                  (minus_1 (V a)) (V b) c' d' e' v0); auto.
+  red. auto.
+  constructor.
+  apply (IHn (minus_1 (V b)) c d e (V a) v2
+                  (minus_1 (V b)) c' d' e' (V a) v6); auto.
+  red. auto.
+  constructor.
+  apply (IHn (minus_1 c) d e (V a) (V b) v3
+                  (minus_1 c') d' e' (V a) (V b) v7); auto.
+  red. auto.
+  constructor.
+  apply (IHn (minus_1 d) e (V a) (V b) c v4
+                  (minus_1 d') e' (V a) (V b) c' v8); auto.
+  red. auto.
+  apply (IHn (minus_1 e) (V a) (V b) c d v5
+                  (minus_1 e') (V a) (V b) c' d' v9); auto.
+  red. auto.
+
+  inversion H0. right. auto.
+  inversion H0. right. auto.
+  right. auto.
+Qed.  
 
 Inductive Tarai_Y (n: nat):
-     oZ -> oZ -> oZ -> oZ -> oZ -> Z -> Prop :=
+     Z' -> Z' -> Z' -> Z' -> Z' -> Z -> Prop :=
 | Tarai_Y_2 a b v
-     (Hterm: ntarai5 n (Some a) (Some b) None None None = (Some v))
+     (Hterm: ntarai5 n (V a) (V b) B B B = (V v))
      (Hle: v <= b):
-     Tarai_Y n (Some a) (Some b) None None None v
+     Tarai_Y n (V a) (V b) B B B v
 | Tarai_Y_3 a b c v
-     (Hterm: ntarai5 n (Some a) (Some b) (Some c) None None = (Some v))
+     (Hterm: ntarai5 n (V a) (V b) (V c) B B = (V v))
      (Hle: v <= c):
-     Tarai_Y n (Some a) (Some b) (Some c) None None v
+     Tarai_Y n (V a) (V b) (V c) B B v
 | Tarai_Y_4 a b c d v
-     (Hterm: ntarai5 n (Some a) (Some b) (Some c) (Some d) None = (Some v))
+     (Hterm: ntarai5 n (V a) (V b) (V c) (V d) B = (V v))
      (Hle: v <= d):
-     Tarai_Y n (Some a) (Some b) (Some c) (Some d) None v.
+     Tarai_Y n (V a) (V b) (V c) (V d) B v.
 (*
 | Tarai_Y_1n a b c d e v:
-     Tarai_Y n None b c d e v -> Tarai_Y n a b c d e v
+     Tarai_Y n B b c d e v -> Tarai_Y n a b c d e v
 | Tarai_Y_2n a b c d e v:
-     Tarai_Y n a None c d e v -> Tarai_Y n a b c d e v
+     Tarai_Y n a B c d e v -> Tarai_Y n a b c d e v
 | Tarai_Y_3n a b c d e v:
-     Tarai_Y n a b None d e v -> Tarai_Y n a b c d e v
+     Tarai_Y n a b B d e v -> Tarai_Y n a b c d e v
 | Tarai_Y_4n a b c d e v:
-     Tarai_Y n a b c None e v -> Tarai_Y n a b c d e v
+     Tarai_Y n a b c B e v -> Tarai_Y n a b c d e v
 | Tarai_Y_5n a b c d e v:
-     Tarai_Y n a b c d None v -> Tarai_Y n a b c d e v.*)
+     Tarai_Y n a b c d B v -> Tarai_Y n a b c d e v.*)
 
 
 Proof.
@@ -210,10 +244,10 @@ Proof.
      apply lem_step_l.
      apply lem_step_r.
      simpl in H5.
-     destruct (ntarai5 n (Some (a - 1)) (Some b) None d e) as [v0|].
+     destruct (ntarai5 n (V (a - 1)) (V b) B d e) as [v0|].
      rewrite (H5 v0); auto. clear H5.
      simpl in H4.
-     destruct (ntarai5 n (Some (b - 1)) None d e (Some a)) as [v1|].
+     destruct (ntarai5 n (V (b - 1)) B d e (V a)) as [v1|].
      rewrite (H4 v1); auto. clear H4.
 
   destruct c as [c|]; destruct d as [d|]; destruct e as [e|].
@@ -227,7 +261,7 @@ Proof.
 *)
 
 Lemma lem_1n: forall n a b c d e v,
-  ntarai5 n a None None None None = Some v -> ntarai5 n a b c d e = Some v.
+  ntarai5 n a B B B B = V v -> ntarai5 n a b c d e = V v.
 Proof.
   intros.
   unfold ntarai5 in H.
@@ -235,7 +269,7 @@ Proof.
 Qed.
 
 Lemma lem_2n: forall n a b c d e v,
-  ntarai5 n a b None None None = Some v -> ntarai5 n a b c d e = Some v.
+  ntarai5 n a b B B B = V v -> ntarai5 n a b c d e = V v.
 Proof.
   intros.
   destruct b.
@@ -246,7 +280,7 @@ Proof.
 Qed.
 
 Lemma lem_3n: forall n a b c d e v,
-  ntarai5 n a b None d e = Some v -> ntarai5 n a b c d e = Some v.
+  ntarai5 n a b B d e = V v -> ntarai5 n a b c d e = V v.
 Proof.
   induction n.
     intros.
@@ -260,7 +294,7 @@ Proof.
 
 
 Lemma lem_1: forall a b, a <= b ->
-   exists n, exists v, Tarai_Y n (Some a) (Some b) None None None v.
+   exists n, exists v, Tarai_Y n (V a) (V b) B B B v.
 Proof.
    intros.
    exists 1%nat. exists b.
@@ -272,7 +306,7 @@ Proof.
 Qed.
 
 Lemma lem_2: forall a b c, a <= c -> b <= c ->
-   exists n, exists v, Tarai_Y n (Some a) (Some b) (Some c) None None v.
+   exists n, exists v, Tarai_Y n (V a) (V b) (V c) B B v.
 Proof.
    intros.
    destruct (Z_le_dec a b).
@@ -294,38 +328,38 @@ Proof.
    apply Tarai_Y_3.
    apply lem_step.
    replace (b + 1 - 1) with b by omega.
-   destruct (lem_1 b b None None None) as [n0 [v ?]]. omega.
+   destruct (lem_1 b b B B B) as [n0 [v ?]]. omega.
    unfold ntarai5.
    simpl.
    destruct (Z_le_dec (b + 1 + 0) b).
    apply False_ind. omega.
 
 Inductive Tarai_value:
-     nat -> oZ -> oZ -> oZ -> oZ -> oZ -> Z -> Prop :=
+     nat -> Z' -> Z' -> Z' -> Z' -> Z' -> Z -> Prop :=
 | Tarai_1 n a b
      (Hmax1: a <= b):
-     Tarai_value (S n) (Some a) (Some b) None None None b
+     Tarai_value (S n) (V a) (V b) B B B b
 | Tarai_2 n a b c v1 v2 v3 vr
      (Hmax1: a <= c)
      (Hmax2: b <= c)
-     (Hrec1: Tarai_value n (Some (a-1)) (Some b) (Some c) None None v1)
-     (Hrec2: Tarai_value n (Some (b-1)) (Some c) None None (Some a) v2)
-     (Hrec3: Tarai_value n (Some (c-1)) None None (Some a) (Some b) v3)
-     (Hrec:  Tarai_value n (Some v1) (Some v2) (Some v3) None None vr):
-     Tarai_value (S n) (Some a) (Some b) (Some c) None None vr
+     (Hrec1: Tarai_value n (V (a-1)) (V b) (V c) B B v1)
+     (Hrec2: Tarai_value n (V (b-1)) (V c) B B (V a) v2)
+     (Hrec3: Tarai_value n (V (c-1)) B B (V a) (V b) v3)
+     (Hrec:  Tarai_value n (V v1) (V v2) (V v3) B B vr):
+     Tarai_value (S n) (V a) (V b) (V c) B B vr
 | Tarai_3 n a b c d v1 v2 v3 v4 vr (Hgt: a > b)
      (Hmax1: a <= d)
      (Hmax2: b <= d)
      (Hmax3: c <= d)
-     (Hrec1: Tarai_value n (Some (a-1)) (Some b) (Some c) (Some d) None v1)
-     (Hrec2: Tarai_value n (Some (b-1)) (Some c) (Some d) None (Some a) v2)
-     (Hrec3: Tarai_value n (Some (c-1)) (Some d) None (Some a) (Some b) v3)
-     (Hrec4: Tarai_value n (Some (d-1)) None (Some a) (Some b) (Some c) v4)
-     (Hrec:  Tarai_value n (Some v1) (Some v2) (Some v3) (Some v4) None vr):
-     Tarai_value (S n) (Some a) (Some b) (Some c) (Some d) None vr.
+     (Hrec1: Tarai_value n (V (a-1)) (V b) (V c) (V d) B v1)
+     (Hrec2: Tarai_value n (V (b-1)) (V c) (V d) B (V a) v2)
+     (Hrec3: Tarai_value n (V (c-1)) (V d) B (V a) (V b) v3)
+     (Hrec4: Tarai_value n (V (d-1)) B (V a) (V b) (V c) v4)
+     (Hrec:  Tarai_value n (V v1) (V v2) (V v3) (V v4) B vr):
+     Tarai_value (S n) (V a) (V b) (V c) (V d) B vr.
 
-Lemma Tarai_1_term: forall n a b v, Tarai_value n (Some a) (Some b) None None None v ->
-  ntarai5 n (Some a) (Some b) None None None = Some v.
+Lemma Tarai_1_term: forall n a b v, Tarai_value n (V a) (V b) B B B v ->
+  ntarai5 n (V a) (V b) B B B = V v.
 Proof.
   intros.
   inversion H.
@@ -337,7 +371,7 @@ Proof.
 Qed.
 
 Lemma lem_safficiency:
-  forall n a b c v, Tarai_value n a b c v -> ntarai3 n (Some a) (Some b) (Some c) = Some v.
+  forall n a b c v, Tarai_value n a b c v -> ntarai3 n (V a) (V b) (V c) = V v.
 Proof.
   intros.
   induction H.
@@ -361,7 +395,7 @@ Proof.
 Qed.
 
 Lemma lem_necessity:
-  forall n a b c v, ntarai3 n (Some a) (Some b) (Some c) = Some v -> Tarai_value n a b c v.
+  forall n a b c v, ntarai3 n (V a) (V b) (V c) = V v -> Tarai_value n a b c v.
 Proof.
   induction n.
   intros.
@@ -382,17 +416,17 @@ Proof.
   generalize (IHn (c - 1) a b).
   intros.
   destruct n.
-  destruct (ntarai3 0 (Some (a - 1)) (Some b) (Some c)) as [v1|]; [| inversion H].
-  destruct (ntarai3 0 (Some (b - 1)) (Some c) (Some a)) as [v2|]; [| inversion H].
+  destruct (ntarai3 0 (V (a - 1)) (V b) (V c)) as [v1|]; [| inversion H].
+  destruct (ntarai3 0 (V (b - 1)) (V c) (V a)) as [v2|]; [| inversion H].
   simpl in H.
   destruct (Z_le_dec v1 v2).
   apply Tarai_a_gt_b_1 with v1; auto.
   omega.
   congruence.
   inversion H.
-  destruct (ntarai3 (S n) (Some (a - 1)) (Some b) (Some c)) as [v1|]; [| inversion H].
-  destruct (ntarai3 (S n) (Some (b - 1)) (Some c) (Some a)) as [v2|]; [| inversion H].
-  destruct (ntarai3 (S n) (Some (c - 1)) (Some a) (Some b)) as [v3|].
+  destruct (ntarai3 (S n) (V (a - 1)) (V b) (V c)) as [v1|]; [| inversion H].
+  destruct (ntarai3 (S n) (V (b - 1)) (V c) (V a)) as [v2|]; [| inversion H].
+  destruct (ntarai3 (S n) (V (c - 1)) (V a) (V b)) as [v3|].
   generalize (IHn v1 v2 v3 v H).
   intro.
   simpl in H.
